31a59f804e72ad1904db64323f1a41ed1ecfdabc
asketsystem <asketsystem1@gmail.com>
2022-01-28 23:36:32 +0000

    Creating an initial empty list to store my blobkchain

---
+++ Blockchain.py
@@ -0,0 +1,23 @@
+class Blockchain(object):
+    def _init_(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new Block and add it to the chain
+
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hasshes the last Block in the chain
+
+        pass
+        
+        
+        
